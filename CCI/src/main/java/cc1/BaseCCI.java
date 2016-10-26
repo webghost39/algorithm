09ca0372ca5c42122caac43f8da3af749b4af81e
yu <yu@yu-desktop>
2016-10-25 23:16:15 -0500

    S1_4
    Change-Id: I2916e52c7be51a183e8d6984ba099d8fd10701a8
    Signed-off-by: yu <yu@yu-desktop>

---
+++ CCI/src/main/java/cc1/BaseCCI.java
@@ -0,0 +1,9 @@
+package cc1;
+
+public class BaseCCI
+{
+	protected static void print(Object o)
+	{
+		System.out.println(o);
+	}
+}
--- CCI/src/main/java/cc1/ch1/S1_4.java
+++ CCI/src/main/java/cc1/ch1/S1_4.java
@@ -1,5 +1,54 @@
 package cc1.ch1;
 
-public class S1_4 {
+import java.util.HashMap;
+import java.util.Map;
 
+import cc1.BaseCCI;
+
+public class S1_4 extends BaseCCI
+{
+	public static void main(String[] arg)
+	{
+		print(isAnagrams("2223", "2332"));
+	}
+
+	public static boolean isAnagrams(String str1, String str2)
+	{
+		if (str1 == null && str2 == null)
+		{
+			return true;
+		}
+		else if ((str1 == null && str2 != null)
+				|| (str1 != null && str2 == null))
+		{
+			return false;
+		}
+		else
+		{
+			if (str1.length() != str2.length())
+			{
+				return false;
+			}
+			char[] str1Chars = str1.toCharArray();
+			char[] str2Chars = str2.toCharArray();
+
+			Map<Character, Integer> map = new HashMap<>();
+			for (char c : str1Chars)
+			{
+				map.put(c, map.getOrDefault(c, 0) + 1);
+			}
+			for (char c : str2Chars)
+			{
+				if (map.get(c) == null || map.get(c) <= 0)
+				{
+					return false;
+				}
+				else
+				{
+					map.put(c, map.get(c) - 1);
+				}
+			}
+			return true;
+		}
+	}
 }
