e1bdc32b7e71cc43e3d5a28c18bcbc934e574731
nijiating <nijiating@nijiating-PC>
2016-10-28 23:44:35 -0500

    implement 1.3
    Change-Id: I8911a803c48d1f7cecc577737ddd327eb3af88c7
    Signed-off-by: nijiating <nijiating@nijiating-PC>

--- CCI/src/main/java/cc1/ch1/S1_3.java
+++ CCI/src/main/java/cc1/ch1/S1_3.java
@@ -1,16 +1,34 @@
 package cc1.ch1;
 
-public class S1_3
+import java.util.HashMap;
+import java.util.Map;
+
+import cc1.BaseCCI;
+
+public class S1_3 extends BaseCCI
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		print(removeDuplicatedChar("adfdeddaasdff"));
 	}
 
 	public static String removeDuplicatedChar(String str)
 	{
-		return null;
+		if (str == null || str.length() < 2)
+			return str;
+		Map<Integer, Boolean> map = new HashMap<>();
+		char[] strChars = str.toCharArray();
+		map.put((int) strChars[0], true);
+		int tail = 1;
+		for (int i = 1; i < strChars.length; i++)
+		{
+			if (map.get((int) strChars[i]) == null)
+			{
+				strChars[tail++] = strChars[i];
+				map.put((int) strChars[i], true);
+			}
+		}
+		return new String(strChars, 0, tail);
 	}
 }
